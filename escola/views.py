d708b25ef778304ca59cf5c36aa63faea64d0170
Elivelton Barbosa <barbosa.elivelton22@gmail.com>
2024-05-31 19:04:30 -0300

    Returning Location header uri after Curso creation

--- escola/views.py
+++ escola/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
 from escola.models import Aluno, Curso, Matricula
+from rest_framework.response import Response
 from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
 
 
@@ -18,6 +19,14 @@ class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location'] = f'{request.build_absolute_uri()}{id}'
 
 
 class MatriculaViewSet(viewsets.ModelViewSet):
